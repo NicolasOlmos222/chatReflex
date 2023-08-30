92b51fd8a5a7735ddd627c183ba7ac45a9f84538
nicolasolmos222 <nicolas0olmos@gmail.com>
2023-08-29 21:20:27 -0300

    primer commit

---
+++ chatReflex/openai_env.py
@@ -0,0 +1,1 @@
+API_KEY = "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"
---
+++ rxconfig.py
@@ -0,0 +1,8 @@
+import reflex as rx
+
+class ChatreflexConfig(rx.Config):
+    pass
+
+config = ChatreflexConfig(
+    app_name="chatReflex",
+)
